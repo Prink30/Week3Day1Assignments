e341588eb56e5a2dd27b710c0c463b899c15cc9c
Prink30 <112071940+Prink30@users.noreply.github.com>
2022-11-19 21:29:29 +0530

    Add files via upload
    Assignment1

---
+++ system/Desktop.java
@@ -0,0 +1,21 @@
+package org.system;
+
+public class Desktop extends Computer {
+	
+	public void desktopSize() {
+		System.out.println("desktop size");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Desktop obj = new Desktop();
+		
+		obj.computerModel();
+		obj.desktopSize();
+		
+		
+
+	}
+
+}
