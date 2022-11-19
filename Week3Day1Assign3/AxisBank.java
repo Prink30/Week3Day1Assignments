d70e3fabdf73f8f9b065366455400b5d528d941b
Prink30 <112071940+Prink30@users.noreply.github.com>
2022-11-19 21:35:07 +0530

    Add files via upload
    Assignment 3

---
+++ Week3Day1Assign3/AxisBank.java
@@ -0,0 +1,17 @@
+package Week3Day1Assign3;
+
+public class AxisBank extends BankInfo {
+	
+	public void deposit() {
+		System.out.println("deposited the amount is 8000");
+	} 
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		AxisBank obj = new AxisBank();
+		
+		obj.deposit();
+	}
+
+}
---
+++ Week3Day1Assign3/BankInfo.java
@@ -0,0 +1,19 @@
+package Week3Day1Assign3;
+
+public class BankInfo {
+	
+
+	
+	public void saving() {
+		System.out.println("savings account");
+	}
+	
+	public void fixed() {
+		System.out.println("fixed deposit");
+	}
+	
+	public void deposit() {
+		System.out.println("deposited the amount is 5000");
+	}
+
+}
